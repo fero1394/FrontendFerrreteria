0611985021c96e46c74b6fcc619c9ebf654bf149
FernandoQuicenoG <fqgomez@misena.edu.co>
2022-04-04 13:51:50 -0500

    se conecta el backend inventario con el frontend

--- src/components/ViewInventory.jsx
+++ src/components/ViewInventory.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+
+import React,{ useState, useEffect } from 'react'
 
 function ViewInventory() {
-  return (
+    const url = 'https://backend-ferreteria-sofka.herokuapp.com/inventario/'
+  
+    const [todos, setTodos] = useState()
+  
+    const fetchApi = async () => {
+        const response = await fetch(url)
+        console.log(response.status)
+  
+        const responseJSON = await response.json()
+        setTodos(responseJSON)
+    }
+  
+    useEffect(() => {
+        fetchApi()
+    }, [])
+  
+    return (
     <div>
         Estas Viendo el inventario
+        <ul>
+            {!todos ? 'cargando...' :
+                todos.map((todo,index) =>{
+                    return <li key={todo.idInventario}>{todo.idInventario} <br/> {todo.producto.idProducto}<br/> {todo.producto.nombreProducto} <br/>  {todo.producto.precio} <br/>{todo.cantidad} <br/>{todo.proveedor.idProveedor}<br/> {todo.proveedor.nombre} <br/>  {todo.proveedor.cedula} <br/>  {todo.proveedor.celular} </li>
+                })
+            }
+        </ul>
 
     </div>
   )
