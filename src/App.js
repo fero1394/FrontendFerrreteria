220c1eacfb0a22951cf142248c1b02136f2c779d
FernandoQuicenoG <fqgomez@misena.edu.co>
2022-04-04 12:07:35 -0500

    creacion de estructura de proyecto

--- src/App.js
+++ src/App.js
@@ -3,10 +3,32 @@ import Logo from './assets/img/logo.png'
 
 function App() {
   return (
-    <div className="div-welcome">
-      <img src={Logo} alt="" />
-      <h1>Reto final - Client Side</h1>
-    </div>
+    <BrowserRouter>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Login />}/>
+        <Route path="Register" element={<Register />} />
+        <Route path='Dashboard' element={<Dashboard/>}> 
+          <Route path='ProductEntry' element={<ProductEntry/>}/>        
+          <Route path='RegisterSupplier' element={<RegisterSupplier/>}/>
+          <Route path='RegisterCustomer' element={<RegisterCustomer/>}/> 
+          <Route path='Sale' element={<Sale/>}/>
+          <Route path='ViewInventory' element={<ViewInventory/>}/>                
+        </Route>
+        <Route 
+          path="*"
+          elemnt={
+            <main style={{ padding: "1rem" }}>
+            <p>No hay nada por Aqui!</p>
+            </main>
+          }
+        />
+      </Routes>
+      <ProtectedRoute>
+      </ProtectedRoute>
+    </AuthProvider>
+  </BrowserRouter>
+
   );
 }
 
